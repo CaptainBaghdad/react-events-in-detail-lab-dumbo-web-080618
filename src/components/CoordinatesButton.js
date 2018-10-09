aee419ef57c9affab743d0a8ee4f77320ca2df98
Jason Pitts <juniorjr083@gmail.com>
2018-10-09 21:30:16 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 
 class CoordinatesButton extends Component{
   
+<<<<<<< HEAD
  handleClick(event){
     let arr = []
     arr.push(event.clientX,event.clientY)
     return this.props.onReceiveCoordinates(arr);
+=======
+ mouseCoordinates(event){
+    let arr = []
+    arrr.push(event.clientX,event.clientY)
+>>>>>>> 000eaf5269a4da4123e9c3123e21a7bce83227b6
   }
   
   render(){
@@ -15,7 +21,11 @@ class CoordinatesButton extends Component{
     
     return(
       <div>
+<<<<<<< HEAD
       <button onClick={this.handleClick.bind(this)}></button>
+=======
+      <button onClick={this.findCoors}></button>
+>>>>>>> 000eaf5269a4da4123e9c3123e21a7bce83227b6
       </div>
       
       )
